64f611013f2dc3261ff35cd4db7e787779f7b139
tiffany-simionescu <tsimionescu87@gmail.com>
2022-10-07 16:21:00 -0400

    Added card images in js file.

---
+++ js/game.js
@@ -0,0 +1,33 @@
+
+
+let data;
+const cardBox = document.getElementById('card-box');
+// const cardBox = $("#card-box");
+
+const readJsonFile = (file, callback) => {
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = () => {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+const loadGame = (data) => {
+    cardBox.innerHTML = '';
+    for (i = 0; i < data.length; i++) {
+        let frontCard = '<div class="card-view front-views"><i class="fa fa-question"></i></div>';
+        let backCard = `<div class="card-view back-views"><img src="${data[i]['card_img']}"></div>`;
+        $('#card-box').append(`<div id="${data[i]['card_id']}" class="card" onclick="checkCard("${data[i]['card_id']}", "${data[i]['card_value']}")" >${frontCard}${backCard}</div>`)
+    }
+}
+
+window.onload = () => {
+    readJsonFile('/json/data.json', (text) => {
+        data = JSON.parse(text);
+        loadGame(data);
+    })
+}
